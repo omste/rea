e47181499cad4cce29e86c2a123c9eb1d31fb3b1
unknown <omrisuleiman15@gmail.com>
2023-02-02 14:32:54 +0100

    better query

--- apps/rea/pages/index.tsx
+++ apps/rea/pages/index.tsx
@@ -2,8 +2,8 @@ import { GetStaticPropsContext } from 'next';
 import { Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import styles from './index.module.css';
-import { gql } from "@apollo/client";
-import client from "../apollo-client";
+import { gql } from '@apollo/client';
+import client from '../apollo-client';
 
 import {
   Logo,
@@ -51,13 +51,15 @@ export function Index({ dataTopNavLinks, pokies }: IndexProps) {
           // leave="ease-in duration-200"
           // leaveFrom="opacity-100 scale-100"
           // leaveTo="opacity-0 scale-95"
-        enter="transition-opacity duration-75"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity duration-150"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-        ><Modal setOpenModal={setShowModal} /></Transition>
+          enter="transition-opacity duration-75"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Modal setOpenModal={setShowModal} />
+        </Transition>
       </>
       {/* {showModal && <Modal setOpenModal={setShowModal} />} */}
     </div>
@@ -69,33 +71,42 @@ export default Index;
 export async function getStaticProps() {
   const { data } = await client.query({
     query: gql`
-    query MyQuery {
-      pokemon_v2_pokemontype(limit: 10) {
-        pokemon_v2_pokemon {
+      query MyQuery {
+        pokemon_v2_pokemon(limit: 10, order_by: { id: asc }) {
           id
+          pokemon_species_id
           name
+          pokemon_v2_pokemonabilities {
+            ability_id
+            pokemon_v2_ability {
+              name
+            }
+          }
+          pokemon_v2_pokemonmoves {
+            move_id
+            pokemon_v2_move {
+              name
+              pokemon_v2_generation {
+                name
+              }
+              pokemon_v2_type {
+                id
+                name
+              }
+            }
+          }
           pokemon_v2_pokemonsprites {
             sprites
-            pokemon_id
-          }
-        }
-        pokemon_v2_type {
-          id
-          name
-          pokemon_v2_berries {
-            id
-            name
           }
         }
       }
-    }
     `,
   });
 
   return {
     props: {
       dataTopNavLinks,
-      pokies: data
+      pokies: data,
     },
- };
+  };
 }
